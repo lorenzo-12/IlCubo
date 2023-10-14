96aa438af2c84f54ca523305943d3913647606c0
lorenzo di filippo <difilippo.1837562@studenti.uniroma1.it>
2023-10-14 16:58:13 +0200

    aggiunta modifica per rotazione

--- index.js
+++ index.js
@@ -4,15 +4,44 @@
 
 //const portrait = window.matchMedia("(orientation: portrait)").matches;
 //console.log(portrait);
-var ang = screen.orientation.angle;
-if (ang==90){
-    document.body.style.setProperty("-webkit-transform", "rotate(-90deg)", null);
-}
+//var ang = screen.orientation.angle;
+//if (ang==90){
+//    document.body.style.setProperty("-webkit-transform", "rotate(-90deg)", null);
+//}
 
-function css_file(){
+//this.screenOrientation.lock();
+
+screen.orientation.addEventListener("change", function(e) {
+    var ang = screen.orientation.angle;
+    var w = 0;
+    var h = 0;
+    w = window.innerWidth;
+    h = window.innerHeight;
+    if (ang==90){
+        h = window.innerWidth;
+        w = window.innerHeight;
+    }
+    if (ang==270){
+        h = window.innerWidth;
+        w = window.innerHeight;
+    }
+    console.log("change event");
     var s = document.getElementById("scriptCSS");
     //Check if viewport is smaller than 768 pixels
-    if(window.innerWidth < 950) {
+    if(w < 600) {
+        console.log("telefono");
+        s.href = "style_smartphone.css";
+    }else { //Else we have a larger screen
+        console.log("pc");
+        s.href = "style.css";
+    }
+});
+
+function css_file(){
+    console.log("onload");
+    var s = document.getElementById("scriptCSS");
+    //Check if viewport is smaller than 768 pixels
+    if(window.innerWidth < 600) {
         console.log("telefono");
         s.href = "style_smartphone.css";
     }else { //Else we have a larger screen
