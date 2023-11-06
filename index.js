27d91c40f1397c219358037e58ebf5c1fb626549
lorenzo di filippo <difilippo.1837562@studenti.uniroma1.it>
2023-11-06 16:08:43 +0100

    new updates

--- index.js
+++ index.js
@@ -1,62 +1,15 @@
+/*
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-//document.body.requestFullscreen();
-//window.screen.orientation.lock("portrait");
-
-//const portrait = window.matchMedia("(orientation: portrait)").matches;
-//console.log(portrait);
-//var ang = screen.orientation.angle;
-//if (ang==90){
-//    document.body.style.setProperty("-webkit-transform", "rotate(-90deg)", null);
-//}
-
-//this.screenOrientation.lock();
-
-screen.orientation.addEventListener("change", function(e) {
-    var ang = screen.orientation.angle;
-    var w = 0;
-    var h = 0;
-    w = window.innerWidth;
+function reportWindowSize() {
     h = window.innerHeight;
-    if (ang==90){
-        h = window.innerWidth;
-        w = window.innerHeight;
-    }
-    if (ang==270){
-        h = window.innerWidth;
-        w = window.innerHeight;
-    }
-    console.log("change event");
-    var s = document.getElementById("scriptCSS");
-    //Check if viewport is smaller than 768 pixels
-    if(w < 600) {
-        console.log("telefono");
-        s.href = "style_smartphone.css";
-    }
-    else if(w < 900){
-        console.log("tablet");
-        s.href = "style_tablet.css";
-    }
-    else { //Else we have a larger screen
-        console.log("pc");
-        s.href = "style.css";
-    }
-});
-
-function css_file(){
-    var ang = screen.orientation.angle;
-    var w = 0;
-    var h = 0;
     w = window.innerWidth;
-    h = window.innerHeight;
-    if (ang==90){
-        h = window.innerWidth;
-        w = window.innerHeight;
-    }
-    if (ang==270){
-        h = window.innerWidth;
-        w = window.innerHeight;
-    }
-    console.log("onload");
+    console.log("h: ",h);
+    console.log("w: ",w);
+    console.log("------------------------");
+
     var s = document.getElementById("scriptCSS");
     //Check if viewport is smaller than 768 pixels
     if(w < 600) {
@@ -72,8 +25,7 @@ function css_file(){
         s.href = "style.css";
     }
 }
+  
+window.onresize = reportWindowSize;
 
-
-function test(){
-    console.log("ciao");
-}
+*/
